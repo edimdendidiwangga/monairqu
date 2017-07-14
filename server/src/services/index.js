d82894c66eb074fd47437e449fb6a95a2cae0155
edimdendidiwangga <edimdendy@gmail.com>
2017-07-14 16:59:09 +0700

    remove server

--- server/src/services/index.js
+++
@@ -1,7 +0,0 @@
-const airquality = require('./airquality/airquality.service.js');
-const sensor = require('./sensor/sensor.service.js');
-module.exports = function () {
-  const app = this; // eslint-disable-line no-unused-vars
-  app.configure(airquality);
-  app.configure(sensor);
-};
